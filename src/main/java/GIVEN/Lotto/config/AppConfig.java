0138f394895c1b3901a85b4da0668162253cced7
Gibeom Nam <rlqja0523@naver.com>
2022-09-27 00:50:24 +0900

    Update Config WebClient Bean

--- src/main/java/GIVEN/Lotto/config/AppConfig.java
+++ src/main/java/GIVEN/Lotto/config/AppConfig.java
@@ -2,6 +2,7 @@ package GIVEN.Lotto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Random;
 
@@ -11,5 +12,10 @@ public class AppConfig {
     @Bean
     public Random random() {
         return new Random();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create("https://www.dhlottery.co.kr/common.do");
     }
 }
