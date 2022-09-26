2d8eeb995edf7ba07b6e108babb4196460701d0c
Gibeom Nam <rlqja0523@naver.com>
2022-09-27 00:58:56 +0900

    Update WinningNumberRepository add method

--- src/main/java/GIVEN/Lotto/winningNumber/repository/WinningNumberRepository.java
+++ src/main/java/GIVEN/Lotto/winningNumber/repository/WinningNumberRepository.java
@@ -4,4 +4,6 @@ import GIVEN.Lotto.winningNumber.entity.WinningNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WinningNumberRepository extends JpaRepository<WinningNumber, Long> {
+
+    WinningNumber findTopByOrderByIdDesc();
 }
