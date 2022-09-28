4737089df667b335ea68a4299433e42ae29c1052
Gibeom Nam <rlqja0523@naver.com>
2022-09-29 02:09:19 +0900

    Add mapper impl

---
+++ src/main/generated/GIVEN/Lotto/winningNumber/mapper/WinningNumberMapperImpl.java
@@ -0,0 +1,13 @@
+package GIVEN.Lotto.winningNumber.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-09-29T01:49:51+0900",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.16 (Azul Systems, Inc.)"
+)
+@Component
+public class WinningNumberMapperImpl implements WinningNumberMapper {
+}
